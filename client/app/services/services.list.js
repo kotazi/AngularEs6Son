bb9475450cf97ffd33c276f5a85ecb634d6d5bb2
kotazi <kotazi@kotazi.com>
2015-03-07 16:46:12 +0900

    Remove SreviceList

--- client/app/services/services.list.js
+++
@@ -1,5 +0,0 @@
-var addServices = angular.module('Es6SonApp.services');
-
-addServices.factory('BeanServices', function($resource)) {
-  return $resource('http://localhost:8000/api/regions', null);
-};
